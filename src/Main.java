f443b25a025f2fdca4ee804232c4c83f59a6997f
Super Oleg <oleg_in@bk.ru>
2020-02-08 00:31:02 +0300

    StreamExample added

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
-import java.io.*;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class Main {
     public static void main(String[] args) {
-        File fromFolder = new File("c:/AmericasCardroom");
-        File destFolder = new File("c:/AmericasCardroom/new");
+        Path fromFolder = Paths.get("c:/AmericasCardroom");
+        Path destFolder = Paths.get("c:/AmericasCardroom/new");
 
-        if (!fromFolder.exists()) {
+        if (!fromFolder.toFile().exists()) {
             System.out.println("Исходная директория не существует");
             System.exit(0);
         } else {
             try {
                 copyFolder(fromFolder, destFolder);
-            } catch (IOException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
                 System.exit(0);
             }
@@ -19,37 +21,63 @@ public class Main {
         System.out.println("Done");
     }
 
-    public static void copyFolder(File fromDir, File toDir) throws IOException {
+    public static void copyFolder(Path fromDir, Path toDir) throws Exception {
 
-        if (fromDir.isDirectory()) {
-            if (!toDir.exists()) {
-                toDir.mkdir();
-                if (!toDir.exists()) {
-                    throw new IOException("Вы должны обладать правами администратора для записи новых файлов");
+        Files.walk(fromDir).filter(path -> Files.isDirectory(path))
+                .filter(path -> fromDir.toFile().equals(path.toFile().getParentFile()))
+                .forEach(path -> {
+                    try {
+                        Files.createDirectories(Paths.get(path.toString().replace(fromDir.toString(), toDir.toString())));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+        Files.walkFileTree(fromDir, new SimpleFileVisitor<>() {
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                        try {
+                            Thread.sleep(1000);
+                            Files.copy(file, Paths.get(file.toString().replace(fromDir.toString(), toDir.toString())));
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                        return FileVisitResult.CONTINUE;
+                    }
                 }
-                System.out.println("Directory copied from "
-                        + fromDir + "  to " + toDir);
-            }
-            String files[] = fromDir.list();
-
-            for (String file : files) {
-                File srcFile = new File(fromDir, file);
-                File destFile = new File(toDir, file);
-                if (!destFile.getParentFile().equals(srcFile)) {
-                    copyFolder(srcFile, destFile);
-                }
-            }
-        } else {
-            InputStream in = new FileInputStream(fromDir);
-            OutputStream out = new FileOutputStream(toDir);
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = in.read(buffer)) > 0) {
-                out.write(buffer, 0, length);
-            }
-            in.close();
-            out.close();
-            System.out.println("File copied from " + fromDir + " to " + toDir);
-        }
+        );
     }
 }
+
+
+//        if (Files.isDirectory(fromDir)) {
+//             if (!Files.exists(toDir)) {
+//                Files.createDirectory(toDir);
+//                if (!Files.exists(toDir)) {
+//                    throw new IOException("Вы должны обладать правами администратора для записи новых файлов");
+//                }
+//                System.out.println("Directory copied from "
+//                        + fromDir + "  to " + toDir);
+//            }
+//            String files[] = fromDir.list();
+//
+//            for (String file : files) {
+//                File srcFile = new File(fromDir, file);
+//                File destFile = new File(toDir, file);
+//                if (!destFile.getParentFile().equals(srcFile)) {
+//                    copyFolder(srcFile, destFile);
+//                }
+//            }
+//        } else {
+//            InputStream in = new FileInputStream(fromDir);
+//            OutputStream out = new FileOutputStream(toDir);
+//            byte[] buffer = new byte[1024];
+//            int length;
+//            while ((length = in.read(buffer)) > 0) {
+//                out.write(buffer, 0, length);
+//            }
+//            in.close();
+//            out.close();
+//            System.out.println("File copied from " + fromDir + " to " + toDir);
+//        }
+//    }
+//}
